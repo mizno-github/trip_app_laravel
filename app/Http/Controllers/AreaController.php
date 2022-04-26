1d53a2781de6ea53a9f5b756284ef94b55c4ec4d
Admin 水野 <mizno911.zeta@gmail.com>
2022-04-26 12:06:55 +0900

    エリア一覧取得機能の作成

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Area;
 
 class AreaController extends Controller
 {
-    //
+    protected $area;
+
+    public function __construct(Area $area)
+    {
+        $this->area = $area;
+    }
+
+    public function areas()
+    {
+        $areas = $this->area->getAll();
+        return $areas;
+    }
 }
